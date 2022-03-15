d0de632d06c628b6b502424ed4f9f39c31557bb3
eSuminski <rctolinski@gmail.com>
2022-03-15 14:31:50 -0400

    finished customer dao unit tests

--- data_access_layer/customer_dal/customer_dao_imp.py
+++ data_access_layer/customer_dal/customer_dao_imp.py
@@ -1,12 +1,26 @@
+from custom_exceptions.nothing_deleted import NothingDeleted
 from data_access_layer.customer_dal.customer_dao_interface import CustomerDAOInterface
 from entities.customer_class import Customer
+from utils.connection_module import connection
 
 
 class CustomerDAOImp(CustomerDAOInterface):
 
     def create_customer_record(self, customer: Customer) -> Customer:
         sql = "insert into customers values(default, %s, %s) returning customer_id"
-
+        cursor = connection.cursor()
+        cursor.execute(sql, (customer.first_name, customer.last_name))
+        connection.commit()
+        returned_id = cursor.fetchone()[0]
+        customer.customer_id = returned_id
+        return customer
 
     def delete_customer_record(self, customer_id: int) -> bool:
-        pass
+        sql = "delete from customers where customer_id = %s"
+        cursor = connection.cursor()
+        cursor.execute(sql, [customer_id])
+        connection.commit()
+        if cursor.rowcount != 0:
+            return True
+        else:
+            raise NothingDeleted("There was no customer with the given Id")
--- tests/dal_tests/customer/test_customer_dao.py
+++ tests/dal_tests/customer/test_customer_dao.py
@@ -4,23 +4,27 @@ from entities.customer_class import Customer
 
 customer_dao = CustomerDAOImp()
 
+
 def test_create_customer_success():
-    customer = Customer(0,"first","last")
+    customer = Customer(0, "first", "last")
     result = customer_dao.create_customer_record(customer)
     assert result.customer_id != 0
 
+
 def test_create_customer_with_malformed_id():
     # if the wrong data type of id is provided in the customer object the method should still work
-    customer = Customer("one", "bad","id")
-    result = customer_dao.create_customer_record()
+    customer = Customer("one", "bad", "id")
+    result = customer_dao.create_customer_record(customer)
     assert result.customer_id != 0
+
 
 def test_delete_customer_success():
     result = customer_dao.delete_customer_record(-1)
     assert result
 
+
 def test_no_customer_to_delete():
-    #catch no customer is deleted by the method
+    # catch no customer is deleted by the method
     try:
         customer_dao.delete_customer_record(-1000)
         assert False
