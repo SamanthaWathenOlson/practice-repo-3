ed69c69de1094723847257d28e9e91237edf1f07
eSuminski <rctolinski@gmail.com>
2022-03-15 14:25:29 -0400

    created connection module

--- data_access_layer/customer_dal/customer_dao_imp.py
+++ data_access_layer/customer_dal/customer_dao_imp.py
@@ -5,7 +5,8 @@ from entities.customer_class import Customer
 class CustomerDAOImp(CustomerDAOInterface):
 
     def create_customer_record(self, customer: Customer) -> Customer:
-        sql = "insert into customers values(default, %s, %s"
+        sql = "insert into customers values(default, %s, %s) returning customer_id"
+
 
     def delete_customer_record(self, customer_id: int) -> bool:
         pass
